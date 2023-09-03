5efb67868515591f18323330bc611f7b20a8e157
ricardoaguiar <barrios.dev@gmail.com>
2023-09-03 17:05:18 +0200

    add cloudflare adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig } from 'astro/config';
-
 import react from '@astrojs/react';
 import solidJs from '@astrojs/solid-js';
 
+import cloudflare from "@astrojs/cloudflare";
+
 // https://astro.build/config
 export default defineConfig({
-	output: 'hybrid',
-	site: 'https://astro-fem-rbadev.netlify.app/',
-	integrations: [react(), solidJs()],
+  output: 'hybrid',
+  site: 'https://astro-fem-rbadev.netlify.app/',
+  integrations: [react(), solidJs()],
+  adapter: cloudflare()
 });
